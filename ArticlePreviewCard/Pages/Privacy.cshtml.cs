be721a62565020b97fdc6b18f86997d81f04fdc6
francescobergonzini <francesco.bergonzini@webion.it>
2026-01-05 17:33:38 +0100

    feat: add initial commit

---
+++ ArticlePreviewCard/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArticlePreviewCard.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
